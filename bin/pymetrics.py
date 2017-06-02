028aa3ce0a08ffedaa7403c03f825d700533a0c0
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-02 13:20:01 -0400

    Added SNMP oid's

--- bin/pymetrics.py
+++ bin/pymetrics.py
@@ -27,6 +27,7 @@ raw_system_cpu_time = '1.3.6.1.4.1.2021.11.52.0'
 percentage_idle_cpu_time = '1.3.6.1.4.1.2021.11.11.0'
 raw_idle_cpu_time = '1.3.6.1.4.1.2021.11.53.0'
 raw_nice_cpu_time = '1.3.6.1.4.1.2021.11.51.0'
+total_cpu_count = '1.3.6.1.2.1.25.3.3.1.2'
 
 # Memory Statistics
 total_swap_size = '1.3.6.1.4.1.2021.4.3.0'
@@ -37,6 +38,21 @@ total_ram_free = '1.3.6.1.4.1.2021.4.11.0'
 total_ram_shared = '1.3.6.1.4.1.2021.4.13.0'
 total_ram_buffered = '1.3.6.1.4.1.2021.4.14.0'
 total_cached_memory = '1.3.6.1.4.1.2021.4.15.0'
+
+# Storage /
+total_disk_space = '1.3.6.1.4.1.2021.9.1.6.1'
+available_disk_space = '1.3.6.1.4.1.2021.9.1.7.1'
+used_disk_space = '1.3.6.1.4.1.2021.9.1.8.1'
+
+# Storage /media/disk1
+total_disk_space_disk1 = '1.3.6.1.4.1.2021.9.1.6.7'
+available_disk_space_disk1 = '1.3.6.1.4.1.2021.9.1.7.7'
+used_disk_space_disk1 = '1.3.6.1.4.1.2021.9.1.8.7'
+
+# Storage /media/disk2
+total_disk_space_disk2 = '1.3.6.1.4.1.2021.9.1.6.8'
+available_disk_space_disk2 = '1.3.6.1.4.1.2021.9.1.7.8'
+used_disk_space_disk2 = '1.3.6.1.4.1.2021.9.1.8.8'
 
 while True:
     for server in server_list.IPs():
